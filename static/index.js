da3349171b5bcb14d2fd39bd2af6f12afb639f7b
impaxton <hey@paxton.rip>
2024-04-14 15:46:48 -0400

    Update index.js

--- static/index.js
+++ static/index.js
@@ -297,7 +297,7 @@ const cloakFavicon = (url) => {
   if (url !== "assets/favicon.ico" && !/^(https?:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,30}/i.test(url)) {
     alert("You must put a valid URL in the URL box.");
     url = "assets/favicon.ico"
-} else if (!/^(https?:\/\/)/.test(url)) {
+} else if (url !== "assets/favicon.ico" && !/^(https?:\/\/)/.test(url)) {
     url = "https://" + url;
   } 
   if (url.trim() == "") {
